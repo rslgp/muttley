897d6c6c0b010a6b0236dca28f5c26784d70de5e
rslgp <rslgp@cin.ufpe.br>
2015-06-25 00:11:37 -0300

    add fila.c

---
+++ AStar/fila.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+// FILA (QUEUE)
+typedef struct no {
+  int key;
+  struct no *next;
+} no;
+ 
+//no *head = NULL, *aux;
+no *aux;
+
+void inserir(no **head, int key) // push
+{
+  no *novo = (no*) malloc(sizeof(no));
+  novo->key = key;
+  novo->next = NULL;
+  if(!(*head)) { aux = *head = novo; }
+  else {
+    aux->next = novo;
+    aux = aux->next;
+  }
+
+}
+ 
+void pop(no **head) { // pop (erase)
+  if((*head)) {
+    no *temp = (*head)->next;
+    free((*head));
+    *head = temp;
+  }
+}
+ 
+int eh_vazia(no **head) { // empty, is_empty
+  return *head == NULL;
+}
+ 
+/* 
+int main() {
+	no *head=NULL;
+  inserir(head,1);
+  inserir(head,2);
+  inserir(head,3);
+  inserir(head,4);
+  pop(head);
+  pop(head);  
+  pop(head);
+  pop(head);    
+  return 0;
+}*/
